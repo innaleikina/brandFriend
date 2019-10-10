11f30a1a28961e0e2d6e041fd20a4975aa811ab8
Inna <designbyinna@gmail.com>
2019-10-10 17:06:50 -0600

    nav works on template

--- App.js
+++ App.js
@@ -22,24 +22,41 @@
 // });
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button,View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import DetailsScreen from "./app/screens/list"
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
-});
 
-export default createAppContainer(AppNavigator);
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
--- app/screens/list.js
+++ app/screens/list.js
@@ -1,1 +1,15 @@
-const dataBrands = require('../data/PreSetBrands.json');
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class DetailsScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>This is the main app with list</Text>
+        </View>
+      );
+    }
+  }
+
+  export default DetailsScreen;
