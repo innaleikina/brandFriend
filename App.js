58956f3914ad56222070985d0fa4338d310227ed
Inna <designbyinna@gmail.com>
2019-10-10 16:25:32 -0600

    start of navigation set up

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-const dataBrands = require('./app/data/preSetBrands.json');
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+// const dataBrands = require('./app/data/preSetBrands.json');
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Brand Friend is here</Text>
-      <Text> {dataBrands.preSetBrands[0]}</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Brand Friend is here</Text>
+//       <Text> {dataBrands.preSetBrands[1]}</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
 });
+
+export default createAppContainer(AppNavigator);
