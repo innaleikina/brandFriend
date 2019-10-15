e3d49548085f74dc42ec3392c9dcc2741f97aad4
Inna <designbyinna@gmail.com>
2019-10-14 21:23:21 -0600

    working on user flow

--- App.js
+++ App.js
@@ -1,38 +1,119 @@
 
 
 import React from 'react';
-import { Button,View, Text } from 'react-native';
+import { Button,View, Text, TextInput
+} from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import ListScreen from "./app/screens/list"
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation-stack';
+import ListScreen from "./src/screens/List"
+import WelcomeScreen from './src/screens/WelcomeScreen'
+import SignUpScreen from './src/screens/SignUpScreen'
+import SignInScreen from './src/screens/SignInScreen'
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen'
+import AuthLoadingScreen from './src/screens/AuthLoadingScreen'
 import Amplify, { Auth } from 'aws-amplify'
 import config from './src/aws-exports'
 Amplify.configure(config)
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
 
+// class WelcomeScreen extends React.Component {
+//   // state = { // 1
+//   //   authCode: ''
+//   // }
+//   // onChangeText(authCode) { // 2
+//   //   this.setState({ authCode })
+//   // }
+//   // signUp() {
+//   //   Auth.signUp({ // 3
+//   //     username: 'myCoolUsername',
+//   //     password: 'MyCoolP@ssword2!',
+//   //     attributes: {
+//   //       phone_number: '+15555555555',
+//   //       email: 'yourcoolemail@gmail.com'
+//   //     }
+//   //   })
+//   //   .then(res => {
+//   //     console.log('successful signup: ', res)
+//   //   })
+//   //   .catch(err => {
+//   //     console.log('error signing up: ', err)
+//   //   })
+//   // }
+//   // confirmUser() { // 4
+//   //   const { authCode } = this.state
+//   //   Auth.confirmSignUp('myCoolUsername', authCode)
+//   //     .then(res => {
+//   //       console.log('successful confirmation: ', res)
+//   //     })
+//   //     .catch(err => {
+//   //       console.log('error confirming user: ', err)
+//   //     })
+//   // }
+
+
+//   // render() {
+//   //   return (
+//   //     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//   //        {/* <View >
+//   //             <Button title='Sign Up' onPress={this.signUp.bind(this)} />
+//   //             <TextInput
+//   //               placeholder='Input Code'
+//   //               onChangeText={value => this.onChangeText(value)}
+//   //             />
+//   //             <Button
+//   //               title='Confirm User'
+//   //               onPress={this.confirmUser.bind(this)}
+//   //             /> */}
+//   //        {/* </View> */}
+//   //       <Text>Welcome Screen</Text>
+//   //       <Button
+//   //         title="Go to Lists"
+//   //         onPress={() => this.props.navigation.navigate('List')}
+//   //       />
+//   //     </View>
+//   //   );
+//   // }
+// }
+const AuthStackNavigator = createStackNavigator({
+  Welcome: {
+    screen: WelcomeScreen,
+    navigationOptions: () => ({
+      title: `Welcome to this App`, // for the header screen
+      headerBackTitle: 'Back'
+    }),
+  },
+  SignUp: {
+    screen: SignUpScreen,
+    navigationOptions: () => ({
+      title: `Create a new account`,
+    }),
+  },
+  SignIn: {
+    screen: SignInScreen,
+    navigationOptions: () => ({
+      title: `Log in to your account`,
+    }),
+  },
+  ForgotPassword: {
+    screen: ForgotPasswordScreen,
+    navigationOptions: () => ({
+      title: `Create a new password`,
+    }),
+  },
+})
 
 
 const RootStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    Details: ListScreen,
+    // Home: WelcomeScreen,
+    // List: ListScreen,
+    AuthLoading: AuthLoadingScreen,
+    Auth: AuthStackNavigator,
+
   },
-  {
-    initialRouteName: 'Home',
-  }
+  // {
+  //   initialRouteName: 'Home',
+  // }
 );
 
 const AppContainer = createAppContainer(RootStack);
---
+++ src/screens/ForgotPasswordScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+    StyleSheet,
+    View,
+    Text,
+  } from 'react-native'
+  export default class ForgotPassword extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>Forgot Password</Text>
+        </View>
+      )
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#aa73b7',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
