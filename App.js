03531981f9a0b8b07f5f32091b105a60710a7ad1
Inna <designbyinna@gmail.com>
2019-10-14 20:29:17 -0600

    aws authentication

--- App.js
+++ App.js
@@ -1,31 +1,13 @@
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
-// const dataBrands = require('./app/data/preSetBrands.json');
 
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Brand Friend is here</Text>
-//       <Text> {dataBrands.preSetBrands[1]}</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
 
 import React from 'react';
 import { Button,View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import ListScreen from "./app/screens/list"
+import Amplify, { Auth } from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
 
 class HomeScreen extends React.Component {
   render() {
--- app/screens/list.js
+++ app/screens/list.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {StyleSheet,ScrollView, View, SafeAreaView, Text, TextInput } from 'react-native';
+import {StyleSheet,ScrollView, View, SafeAreaView, Text, TextInput, Button,  } from 'react-native';
 const dataBrands = require('../data/preSetBrands.json');
 import Constants from 'expo-constants';
 
@@ -15,19 +15,22 @@ class ListScreen extends React.Component {
     render() {
         const brandsList = this.state.brandsArr.map((data) => {
             return (
-                <Text>{data}</Text>
+                <Text key={data} style={styles.listItem}> {data} </Text>
             )
           })
       return (
         <SafeAreaView style={styles.container}>
-                      <Text>This is the main app with list</Text>
-                      <TextInput style={styles.textInput}
-                                 placeholder="Brand name"
-                                 onChangeText={(text) => this.setState({text})}
-                                 value={this.state.text}/>   
+            <Text>This is the main app with list</Text>
+            <View style={styles.view}>
+                    <TextInput style={styles.textInput}
+                                    placeholder="Brand name"
+                                    onChangeText={(text) => this.setState({text})}
+                                    value={this.state.text}/>   
+                    <Button title="search"> </Button>    
+            </View>     
 
-             <ScrollView style={styles.scrollView}>
-               <Text> {brandsList} </Text>
+             <ScrollView  style={styles.scrollView}>
+              {brandsList}
              </ScrollView>
         </SafeAreaView>
 
@@ -43,12 +46,16 @@ class ListScreen extends React.Component {
       alignContent:"center",
       alignItems:"center",
       justifyContent:"center",
-      borderColor:"red",
-      borderWidth: 0.5,
+    //   borderColor:"red",
+    //   borderWidth: 0.5,
     },
     scrollView: {
-      backgroundColor: 'pink',
-      marginHorizontal: 20,
+    //   backgroundColor: 'pink',
+    //   marginHorizontal: 20,
+    width:"100%",
+    display:"flex",
+    flexDirection:"column",
+    // alignItems:"center"
     },
     text: {
       fontSize: 42,
@@ -57,12 +64,34 @@ class ListScreen extends React.Component {
         borderRadius: 4,
         borderWidth: 0.5,
         borderColor: '#d6d7da',
-        width:"50%",
+        width:"60%",
         marginTop:40,
         marginBottom:40
       
-    }
+    },
+
+     listItem: {
+       marginTop:14,
+       marginBottom:14,
+       color:"purple",
+        borderBottomColor: 'gray',
+       borderBottomWidth: 1,
+       marginLeft:80
+
+     },
+     view: {
+         display:"flex",
+        //  borderRadius: 4,
+        //  borderWidth: 0.5,
+        //  borderColor: 'red',
+         flexDirection:"row",
+         justifyContent:"center",
+         alignItems:"center",
+         marginTop:40
+     }
 
   });
+
+  
 
   export default ListScreen;
